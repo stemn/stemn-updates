4dc9d139464fcb3de55e2f11b933d7ca1ff91f79
DaveRevay2 <daverevay@gmail.com>
2016-12-03 21:24:16 +1100

    release

--- update.js
+++ update.js
@@ -6,28 +6,58 @@ const repo = 'Stemn/Stemn-Desktop';
 const rootPath = './';
 const releaseFilePath = path.join(rootPath, 'updates.json');
 
+const appName = 'Stemn'
+const owner   = 'Stemn';
+const repo    = 'Stemn-Desktop';
+const domain  = 'https://github.com';
+
 const getFileData = (tag) => {
   return {
     "linux-x64-prod": {
-      "update": `https://github.com/Stemn/Stemn-Desktop/releases/download/v${tag}/Stemn-${tag}-x86_64.AppImage`,
-      "version": tag,
-      "platform": "linux",
-      "readme": ""
+      "update"   : `${domain}/{${owner}/${repo}/releases/download/v${tag}/${appName}-${tag}-x86_64.AppImage`,
+      "install"  : `${domain}/{${owner}/${repo}/releases/download/v${tag}/${appName}-${tag}-x86_64.AppImage`,
+      "version"  : tag,
+      "platform" : "linux",
+      "readme"   : ""
     },
     "win32-x64-prod": {
-      "update": `https://github.com/Stemn/Stemn-Desktop/releases/download/v${tag}`,
-      "version": tag,
-      "platform": "win32",
-      "readme": ""
+      "update"   : `${domain}/{${owner}/${repo}/releases/download/v${tag}`,
+      "install"  : `${domain}/{${owner}/${repo}/releases/download/v${tag}/${appName}-Setup-${tag}.exe`,
+      "version"  : tag,
+      "platform" : "win32",
+      "readme"   : ""
     },
     "darwin-x64-prod": {
-      "update": `https://github.com/Stemn/Stemn-Desktop/releases/download/v${tag}/Stemn-${tag}-mac.zip`,
-      "version": tag,
-      "platform": "darwin",
-      "readme": ""
+      "update"   : `${domain}/{${owner}/${repo}/releases/download/v${tag}/release.json`,
+      "install"  : `${domain}/{${owner}/${repo}/releases/download/v${tag}/Stemn-${tag}.dmg`,
+      "version"  : tag,
+      "platform" : "darwin",
+      "readme"   : ""
     }
   }
 }
+
+
+{
+  "linux-x64-prod": {
+    "readme": "Second release",
+    "update": "https://github.com/megahertz/electron-simple-updater/releases/download/linux-x64-prod-v0.0.2/simple-updater-example-0.0.2-x86_64.AppImage",
+    "install": "https://github.com/megahertz/electron-simple-updater/releases/download/linux-x64-prod-v0.0.2/simple-updater-example-0.0.2-x86_64.AppImage",
+    "version": "0.0.2"
+  },
+  "win32-x64-prod": {
+    "readme": "Second release",
+    "update": "https://github.com/megahertz/electron-simple-updater/releases/download/win32-x64-prod-v0.0.2",
+    "install": "https://github.com/megahertz/electron-simple-updater/releases/download/win32-x64-prod-v0.0.2/Simple.Updater.Example.Setup.0.0.2.exe",
+    "version": "0.0.2"
+  },
+  "darwin-x64-prod": {
+    "readme": "Second Release",
+    "update": "https://github.com/megahertz/electron-simple-updater/releases/download/darwin-x64-prod-v0.0.2/release.json",
+    "version": "0.0.2"
+  }
+}
+
 
 console.log('Fetching latest version from github...');
 http({
