78b22f7c1084033d991bd8dc39ab097f7bb8625c
DaveRevay2 <daverevay@gmail.com>
2016-12-03 21:25:54 +1100

    release

--- update.js
+++ update.js
@@ -1,7 +1,6 @@
 const fs   = require('fs');
 const path = require('path');
 const http = require('axios');
-const repo = 'Stemn/Stemn-Desktop';
 
 const rootPath = './';
 const releaseFilePath = path.join(rootPath, 'updates.json');
@@ -37,32 +36,10 @@ const getFileData = (tag) => {
   }
 }
 
-
-{
-  "linux-x64-prod": {
-    "readme": "Second release",
-    "update": "https://github.com/megahertz/electron-simple-updater/releases/download/linux-x64-prod-v0.0.2/simple-updater-example-0.0.2-x86_64.AppImage",
-    "install": "https://github.com/megahertz/electron-simple-updater/releases/download/linux-x64-prod-v0.0.2/simple-updater-example-0.0.2-x86_64.AppImage",
-    "version": "0.0.2"
-  },
-  "win32-x64-prod": {
-    "readme": "Second release",
-    "update": "https://github.com/megahertz/electron-simple-updater/releases/download/win32-x64-prod-v0.0.2",
-    "install": "https://github.com/megahertz/electron-simple-updater/releases/download/win32-x64-prod-v0.0.2/Simple.Updater.Example.Setup.0.0.2.exe",
-    "version": "0.0.2"
-  },
-  "darwin-x64-prod": {
-    "readme": "Second Release",
-    "update": "https://github.com/megahertz/electron-simple-updater/releases/download/darwin-x64-prod-v0.0.2/release.json",
-    "version": "0.0.2"
-  }
-}
-
-
 console.log('Fetching latest version from github...');
 http({
   method: 'GET',
-  url: `https://api.github.com/repos/${repo}/releases/latest`
+  url: `https://api.github.com/repos/${owner}/${repo}/releases/latest`
 }).then(response => {
   const tagName = response.data.tag_name.startsWith('v') ? response.data.tag_name.substring(1) : response.data.tag_name;
   console.log('Latest version: ' + tagName);
